b31e3961465cf33aebaea0371ec6f16b817271c8
Kalyan Gandikota <viewlift@Snags-MacBook-Pro-3.local>
2018-04-16 17:30:01 -0400

    initail commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Kalyan from './modules/kalyan'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Kalyan />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/modules/kalyan/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Kalyan extends Component {
+  render() {
+    return (
+      <div className="App">
+       Kalyan
+      </div>
+    );
+  }
+}
+
+module.exports = Kalyan;
